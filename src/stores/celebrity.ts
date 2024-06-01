1f3190c4a4a0f3928795923400539cf6bdd592a6
Lvscls <lilianvscls.pro@gmail.com>
2024-06-01 15:02:17 +0200

    chore: Refactor celebrity store and add game store

--- src/stores/celebrity.ts
+++ src/stores/celebrity.ts
@@ -1,19 +1,19 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
-import { Celebrity } from '../../types'
+import type { Celebrity } from '../types'
 
 export const useCelebrityStore = defineStore('celebrity', {
-    state: () => ({
-        celebrity: null
-    }),
-    actions: {
-        async getRandomCelebrity() {
-            try {
-                const response = await axios.get<Celebrity>('http://127.0.0.1:5000/random')  
-                this.celebrity = response.data          
-            } catch (error) {
-                console.error(error)
-            }
-        }
+  state: () => ({
+    celebrity: null
+  }),
+  actions: {
+    async getRandomCelebrity() {
+      try {
+        const response = await axios.get<Celebrity>('http://127.0.0.1:5000/random')
+        this.celebrity = response.data
+      } catch (error) {
+        console.error(error)
+      }
     }
+  }
 })
---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+export type  Celebrity = {
+  name: string
+  birth: string
+  gender: string
+  occupation: string
+  nationality: string
+  image: string
+}
+
+export type Hint = {
+  correct: boolean
+  name: string
+  birthdate: string
+  gender: string
+  occupation: string
+  nationality: string
+}
